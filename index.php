b8b5b1dc1c2bbb960bf6b6cf6a59e18beea50a02
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-24 00:24:19 +0300

    fontawesome cdn was disabled

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width">
     <title>Library</title>
 
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+<!--    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">-->
     <link rel="stylesheet" href="css/table.css">
     <link rel="stylesheet" href="css/tooltip.css">
 
