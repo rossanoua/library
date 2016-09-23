b270f6a5f03fc4113451a21538e1feb9b5ee18da
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-23 15:27:24 +0300

    tooltip was added maybe not working on mobile devices

--- table.php
+++ table.php
@@ -1,4 +1,4 @@
 <?php
 foreach (glob("files/*.zip") as $filename) {
-    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><i class=\"fa fa-info-circle\" aria-hidden=\"true\"><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></i></td></tr>";
+    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><abbr title=" . str_replace('files', '', $filename) .  " rel=\"tooltip\"> <i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> </abbr><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></td></tr>";
 }
