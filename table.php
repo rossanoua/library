7548a8e6f2309480a2eec58139036f8b01bad003
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-23 21:10:41 +0300

    script search and generate the page for *.fb2 files. tooltip adjusted

--- table.php
+++ table.php
@@ -1,4 +1,4 @@
 <?php
-foreach (glob("files/*.zip") as $filename) {
-    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><abbr title=" . str_replace('files', '', $filename) .  " rel=\"tooltip\"> <i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> </abbr><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></td></tr>";
+foreach (glob("files/*.fb2") as $filename) {
+    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><abbr title=" . preg_replace('/\s+/', '_', str_replace('files/', '', $filename)) .  " rel=\"tooltip\"> <i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> </abbr><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></td></tr>";
 }
