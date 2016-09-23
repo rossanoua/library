fbdfdfb6db81d48137599d8324eb8b6e947d0abf
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-23 21:55:25 +0300

    fontawesome element was deleted text 'tap me' was added insteed

--- table.php
+++ table.php
@@ -1,4 +1,4 @@
 <?php
 foreach (glob("files/*.fb2") as $filename) {
-    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><abbr title=" . preg_replace('/\s+/', '_', str_replace('files/', '', $filename)) .  " rel=\"tooltip\"> <i class=\"fa fa-info-circle\" aria-hidden=\"true\"></i> </abbr><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></td></tr>";
+    echo "<tr><td class=\"tg-431l first\">" . $size->sizeUnits(filesize($filename)) . "</td><td class=\"tg-431l second\"><abbr title=" . preg_replace('/\s+/', '_', str_replace('files/', '', $filename)) .  " rel=\"tooltip\"> tap me </i> </abbr><a href=\"$filename\">" . str_replace('files/', '', $filename) . "</a></td></tr>";
 }
